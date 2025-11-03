bf41ab2ab9708bf4be4fda889f5c4f3633de71c1
Aravind <kolanuaravindreddy@gmail.com>
2025-11-03 18:24:09 +0530

    sg-rules-infra

---
+++ 30-sg-rules/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  frontend_sg_id=data.aws_ssm_parameter.frontend_sg_id.value
+  frontend_alb_sg_id=data.aws_ssm_parameter.frontend_alb_sg_id.value
+  backend_alb_sg_id=data.aws_ssm_parameter.backend_alb_sg_id.value
+  bastion_sg_id=data.aws_ssm_parameter.bastion_sg_id_id.value
+  }
---
+++ 30-sg-rules/main.tf
@@ -0,0 +1,11 @@
+
+#backend_alb accepting traffic from bastion
+resource "aws_security_group_rule" "backend_alb_bastion" {
+    type = "ingress"
+    security_group_id = local.backend_alb_sg_id
+    source_security_group_id = local.bastion_sg_id
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  
+}
