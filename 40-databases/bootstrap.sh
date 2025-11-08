0e7ece56fe73f4a02f7fafc00a4879d70e44ad97
Aravind <kolanuaravindreddy@gmail.com>
2025-11-08 15:21:29 +0530

    databases

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf install ansible -y
-ansible-pull -U https://github.com/cloudenthusiast-tech/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/cloudenthusiast-tech/ansible-roboshop-roles-tf.git -i inventory.ini -e component=mongodb main.yaml
