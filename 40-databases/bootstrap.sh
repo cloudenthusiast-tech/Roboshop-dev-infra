f7bc61b046280c14a041c523b0561fd80bf36ef1
Aravind <kolanuaravindreddy@gmail.com>
2025-11-08 19:37:44 +0530

    databases

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,29 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/cloudenthusiast-tech/ansible-roboshop-roles-tf.git -i inventory.ini -e component=$component main.yaml
+#ansible-pull -U https://github.com/cloudenthusiast-tech/ansible-roboshop-roles-tf.git -i inventory.ini -e component=$component main.yaml
+
+REPO_URL=https://github.com/cloudenthusiast-tech/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
+
+cd $REPO_DIR
+
+#check if ansible repo is already cloned or not
+
+if [ -d $ANSIBLE_DIR ]; then
+
+    cd $ANSIBLE_DIR
+    git pull
+else
+    git clone $REPO_URL
+    cd $REPO_DIR
+fi
+
+ansible-playbook -e component=$component main.yaml
+    
+    
