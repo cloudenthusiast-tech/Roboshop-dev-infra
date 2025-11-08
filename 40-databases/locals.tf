882bdb09c63701c5cf91f8b0f1e34ea492cba581
Aravind <kolanuaravindreddy@gmail.com>
2025-11-08 15:31:36 +0530

    databases

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,9 +1,10 @@
 locals {
     common_name_suffix="${var.project_name}-${var.environment}"
-  ami_id= data.aws_ami.devops_ami_id.id
-  mongodb_sg_id=data.aws_ssm_parameter.mongodb_sg_id.value
-  database_subnet_id=split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
-   common_tags={
+    ami_id= data.aws_ami.devops_ami_id.id
+    mongodb_sg_id=data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id=data.aws_ssm_parameter.redis_sg_id.value
+    database_subnet_id=split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
+    common_tags={
     project_name= var.project_name
     environment= var.environment
     terraform= true
