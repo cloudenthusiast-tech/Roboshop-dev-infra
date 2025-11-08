cb221a3116d43e18c981730dfd9858961441266e
Aravind <kolanuaravindreddy@gmail.com>
2025-11-08 13:20:39 +0530

    databases

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,7 +2,7 @@ locals {
     common_name_suffix="${var.project_name}-${var.environment}"
   ami_id= data.aws_ami.devops_ami_id.id
   mongodb_sg_id=data.aws_ssm_parameter.mongodb_sg_id.value
-  database_subnet_ids=split("," , data.aws_ssm_parameter.database_subnet_ids.value)
+  database_subnet_id=split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
    common_tags={
     project_name= var.project_name
     environment= var.environment
