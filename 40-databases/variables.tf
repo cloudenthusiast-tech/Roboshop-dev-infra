04babcf4f780574e869964d322a07236d3e16270
Aravind <kolanuaravindreddy@gmail.com>
2025-11-09 18:15:49 +0530

    databases

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,12 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z10446792H6QP6XMMVXFH"
+}
+
+variable "domain_name" {
+    default = "kolanu.space"
+  
+}
