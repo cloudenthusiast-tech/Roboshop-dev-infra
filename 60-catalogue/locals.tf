b0a016da628c0c03e99d3aa9f27c177f386ba841
Aravind <kolanuaravindreddy@gmail.com>
2025-11-09 18:27:06 +0530

    catalogue

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_name_suffix="${var.project_name}-${var.environment}"
+  ami_id=data.aws_ami.devops_ami_id.value
+  private_subnet_id=split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  catalogue_sg_id=data.aws_ssm_parameter.catalogue_sg_id.value
+  common_tags={
+    project_name= var.project_name
+    environment= var.environment
+    terraform= true
+}
+}
---
+++ 60-catalogue/main.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "catalogue" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.catalogue_sg_id]
+    subnet_id = local.private_subnet_id
+    
+    tags=merge(
+        local.common_tags,
+        {
+            Name= "${var.project_name}-${var.environment}-catalogue"
+        }
+    )
+}
+
+
+resource "terraform_data" "catalogue" {
+    triggers_replace = [    # triggered only when instanced id changed
+        aws_instance.catalogue.id
+    ]
+
+connection {
+  type= "ssh"
+  user= "ec2-user"
+  password = "DevOps321"
+  host= aws_instance.catalogue.private_ip
+}
+
+#terraform copies this file to mysql server
+provisioner "file" {
+    source = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  
+}
+
+provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/catalogue.sh",
+        "sudo sh /tmp/bootstrap.sh catalogue ${var.environment}"
+     ]
+   } 
+}
